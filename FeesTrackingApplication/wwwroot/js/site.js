cda56a0a7703c26dfde6e3a8ba97cea3f8fad6d4
Aniket <aniketbhuse21@gmail.com>
2025-12-30 17:46:24 +0530

    first commit

---
+++ FeesTrackingApplication/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    if (navMenu) navMenu.classList.toggle('active');
+});
